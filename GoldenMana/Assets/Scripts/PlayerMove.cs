a8973e6fb3b44a4341a158676817133a3ce9116e
Pacnatz <nhatchu04@yahoo.com>
2024-11-07 22:54:06 -0600

    Fixed movement
    Should jump 3 units high, should jump gap 4x3 units as well

--- GoldenMana/Assets/Scripts/PlayerMove.cs
+++ GoldenMana/Assets/Scripts/PlayerMove.cs
@@ -14,6 +14,8 @@ public class PlayerMove : MonoBehaviour
 
     private bool onFloor = false;
     private bool isJumping = false;
+    private float startJumpHeight;
+    
 
     private void Awake() {
         if (Instance != null) {
@@ -43,44 +45,53 @@ public class PlayerMove : MonoBehaviour
     private void HandleMovement() {
 
         // Test floor
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, .7f, floorLayer);
+        float playerWidth = .25f;
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, playerWidth, Vector2.down, .7f, floorLayer);  // Distance is player height / 2 + 0.2
         if (hit) { onFloor = true; }
         else { onFloor = false; }
 
         // Handle Jump
+        float jumpDeceleration;
+        float minJumpHeight = 1;
         if (isJumping) {
-            float jumpDeceleration = 5f;
-            rb.linearVelocityY -= jumpDeceleration * Time.deltaTime;
 
-            if (rb.linearVelocityY <= 0) { //If decelerated to 0
-                isJumping = false;
+            if (transform.position.y < startJumpHeight + minJumpHeight) {
+                float jumpSpeed = 6f;
+                rb.linearVelocityY = jumpSpeed;
             }
+            else{
+                jumpDeceleration = 8.9f;
+                rb.linearVelocityY -= jumpDeceleration * Time.deltaTime;
+            }
+            if (rb.linearVelocityY <= 0) { //If decelerated to 0
+                    isJumping = false;
+            }
+            
         }
         else {
-            float minVelocityY = -10f;
-            float fallSpeed = 1.5f;
+            float minVelocityY = -11f;
+            float fallSpeed = 2f;
             rb.linearVelocityY = Mathf.Lerp(rb.linearVelocityY, minVelocityY, fallSpeed * Time.deltaTime);
         }
 
-        // Handle X Movement
-
+        // Handle X Movement *slidy x movement*
         if (moveDir != 0) {  // Acceleration
             float accelerationRate = 10f;
-            float decelerationRate = 30f;
-            float maxMoveSpeed = 4f;
+            float opposingDecelerationRate = 30f; // Deceleration rate when input is in opposite direction
+            float maxMoveSpeed = 4.3f;
             if (rb.linearVelocityX > 0 && moveDir < 0) { // Handle opposite XMovement
-                rb.linearVelocityX += moveDir * decelerationRate * Time.deltaTime;
+                rb.linearVelocityX += moveDir * opposingDecelerationRate * Time.deltaTime;
             }
             else if (rb.linearVelocityX < 0 && moveDir > 0) { // Handle opposite XMovement
-                rb.linearVelocityX += moveDir * decelerationRate * Time.deltaTime;
+                rb.linearVelocityX += moveDir * opposingDecelerationRate * Time.deltaTime;
             }
-            else {
+            else {  // Handle acceleration
                 rb.linearVelocityX += moveDir * accelerationRate * Time.deltaTime;
             }
             rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxMoveSpeed, maxMoveSpeed);
         }
         else {  // Deceleration
-            float decelerationRate = 20f;
+            float decelerationRate = 20f;  // Decleration rate when no input
             if (rb.linearVelocityX > 0) {
                 rb.linearVelocityX -= decelerationRate * Time.deltaTime;
             }
@@ -94,12 +105,11 @@ public class PlayerMove : MonoBehaviour
 
     private void Input_OnJumpPressed(object sender, System.EventArgs e) {
         if (onFloor) {
-            if (rb != null) { // For some reason you need this test or bad things will happen
+            if (rb != null) {
                 isJumping = true;
-                float jumpSpeed = 6f;
-                rb.linearVelocityY = jumpSpeed;
+                startJumpHeight = transform.position.y;
             }
-            
+
         }
     }
     private void Input_OnJumpCanceled(object sender, System.EventArgs e) {
